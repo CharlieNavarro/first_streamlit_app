2e44a6f71d222942b18c15408fa9b1f8abeb15c8
CharlieNavarro <134897103+CharlieNavarro@users.noreply.github.com>
2023-05-31 09:47:37 -0400

    indent fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,9 +28,9 @@ streamlit.dataframe(fruits_to_show)
 
 def get_gruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    return fruityvice_normalized
-    #    streamlit.write('The user entered ', fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+#    streamlit.write('The user entered ', fruit_choice)
 
 streamlit.header("Fruityvice Fruit Advice!")
 try:
